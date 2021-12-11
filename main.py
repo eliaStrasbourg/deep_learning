e9bba51e2a576cd834e9c83c2d90504cc33fdb6b
elia <elia.belkacem@epitech.eu>
2021-12-11 17:35:15 +0100

    [ADD] Layout display by column

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ import tensorflow_datasets as tfds
 from tensorflow import keras
 from tensorflow.keras import layers
 
-st.header('Input part')
-st.write('Select the picture to test')
-st.write('mosaic for the picture')
-st.header('Result of the analyze')
-st.write('For this picture:')
-st.write('display the picture')
-st.write('Our AI deterined this tree:')
-st.write('The real result is :')
-st.write('The result is good / is not')
+
+# Layout
+
+st.set_page_config(layout="wide")
+input_col, AI_col = st.columns([1, 2])
+
+with input_col:
+    st.header('Input part')
+    st.write('Select the picture to test')
+    st.write('mosaic for the picture')
+with AI_col:
+    st.header('Result of the analyze')
+    st.write('For this picture:')
+    st.write('display the picture')
+    st.write('Our AI deterined this tree:')
+    st.write('The real result is :')
+    st.write('The result is good / is not')
 
