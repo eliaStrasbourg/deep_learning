a8e08cd1f4cc5bbb485fc69e354487fa476b5949
romain <romain.guillaume@epitech.eu>
2021-12-12 03:47:47 +0100

    trying to select and display a specific image

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ with C2:
     
     option = st.selectbox(
      'How would you like to be contacted?',
-     (st.image(random_image, width=100))
+     (st.image(random_image, width=100)))
 
     st.write('You selected:')
     st.image(option)
