5f11e2741dd37dd74395c08cee30f81f50e28c2e
elia <elia.belkacem@epitech.eu>
2021-12-12 16:24:55 +0100

    teston

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ def main():
                 img = mpimg.imread(random_image[y + x * 3])
                 ax[x, y].imshow(img)
                 ax[x, y].set_title([y + x * 3 + 1])
+                ax[x, y].axis('off')
         st.pyplot(fig)
 
         result = st.button('Test AI')
