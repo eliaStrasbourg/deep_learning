9956aa512dce416780e5975af87c2d3757d498e3
LysWalkorkill <50707000+LysWalkorkill@users.noreply.github.com>
2021-12-12 17:13:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import glob, random
 import matplotlib.image as mpimg
-import SessionState
 from tensorflow import keras
 from tensorflow.keras import layers
 
@@ -17,16 +16,15 @@ st.set_page_config(layout="wide")
 input_col, AI_col = st.columns([1, 2])
 good_tree = ["Azadirachta Indica","Carissa Carandas", "Ficus Religiosa"]
 
-#random_image = []
-random_image = SessionState.get(img=[])
+st.session_state['random_image'] = []
 i = 1
 while i < 10:
 	file_path_type = ["./leaf dataset/Azadirachta Indica (Neem)/*.jpg", "./leaf dataset/Carissa Carandas (Karanda)/*.jpg", "./leaf dataset/Ficus Religiosa (Peepal Tree)/*jpg"]
 	images = glob.glob(random.choice(file_path_type))
-	random_image.img.insert(0, random.choice(images))
+	st.session_state.random_image.insert(0, random.choice(images))
 	i += 1
-pick_img = SessionState.get(img=[])
-pick_img.img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image.img) + 1)])
+st.session_state['pick_img'] = []
+st.session_state.pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(st.session_state.random_image) + 1)])
 #pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image) + 1)])
 
 def tree(witch_tree):
@@ -49,7 +47,7 @@ def main():
 
         for x in range(3):
             for y in range(3):
-                img = mpimg.imread(random_image.img[y + x * 3])
+                img = mpimg.imread(st.session_state.random_image[y + x * 3])
                 ax[x, y].imshow(img)
                 #ax[x, y].set_title(y + x * 3 + 1)
                 ax[x, y].axis('off')
@@ -63,8 +61,8 @@ def main():
         with AI_col:
             st.header('Result of the analyze')
             st.write('For this picture:')
-            st.image(random_image[pick_img - 1], width=200)
-            witch_tree = random_image[pick_img - 1][15:16]
+            st.image(st.session_state.random_image.[pick_img - 1], width=200)
+            witch_tree = random_image[st.session_state.pick_img - 1][15:16]
             st.write('Our AI deterined this tree:')
             ia_tree = ""
             st.write(ia_tree)
