06404a9b241d91fa8b02791d2b234cb91fe09bde
LysWalkorkill <50707000+LysWalkorkill@users.noreply.github.com>
2021-12-12 15:32:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
             random_image.insert(0, random.choice(images))
         pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image) + 1)])
         st.write('mosaic for the picture')
-	for x in range(1, len(random_image)):
+	for x in range(1, 10):
 	    st.image(random_image[x], width=100, caption=x)
 
         result = st.button('Test AI')
