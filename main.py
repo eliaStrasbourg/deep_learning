f093b6d51cb9cbe675c3b0ed20ea71967686df3b
romain <romain.guillaume@epitech.eu>
2021-12-12 04:34:24 +0100

    trying to select and display a specific image

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import glob, random
-import streamlit.components.v1 as components
 from tensorflow import keras
 from tensorflow.keras import layers
 
@@ -12,52 +12,30 @@ from tensorflow.keras import layers
 # Layout
 
 st.set_page_config(layout="wide")
-C1, C2, C3 = st.columns(3)
+input_col, AI_col = st.columns([1, 3])
 
-def main():
+with input_col:
+    st.header('Input part')
+    st.write('Select the picture to test')
     random_image = []
-    for i in range(1, 11):
+    for i in range(1, 10):
         file_path_type = ["./leaf dataset/Azadirachta Indica (Neem)/*.jpg", "./leaf dataset/Carissa Carandas (Karanda)/*.jpg", "./leaf dataset/Ficus Religiosa (Peepal Tree)/*jpg"]
         images = glob.glob(random.choice(file_path_type))
         random_image.insert(0, random.choice(images))
+    pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image))])
+    st.write('mosaic for the picture')
+    st.image(random_image, width=100)
 
-    with C2:
-        st.markdown("<h1 style='text-align: center; color: white;'>Input part</h1>", unsafe_allow_html=True)
-        #st.header('Input part')
-        st.write('Select the picture to test')
-        #pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image))])
-        st.write('mosaic for the picture')
-        #st.image(random_image, width=100)
-        
-        selected_image = []
-        st.subheader('select one image')
-        for x in range(1, len(random_image)):
-            st.image(random_image[x], caption=x, width=100)
-            selected_image = st.checkbox(random_image[x])
-            if selected_image:
-                selected_image.append(random_image[x])
-
-        submit = st.button('submit image')
-        if submit:
-            st.image(selected_image, width=100)
-
-
-if __name__ == "__main__":
-    main()
-
-
-#if pick_img:
-#    st.header('Result of the analyze')
-#    st.write('For this picture:')
-#    print(pick_img)
-#    st.write('display the picture')
-#    st.write('Our AI deterined this tree:')
-#    st.write('The real result is :')
-#    st.write('The result is good / is not')
+    if pick_img:
+        with AI_col:
+            st.header('Result of the analyze')
+            st.write('For this picture:')
+            print(pick_img)
+            st.write('display the picture')
+            st.write('Our AI deterined this tree:')
+            st.write('The real result is :')
+            st.write('The result is good / is not')
 
 
 
-#    st.button('Test AI')
-
-
-
+    st.button('Test AI')
