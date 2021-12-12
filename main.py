ef5b26133cee48f996ef570ab774b5647b8dd974
LysWalkorkill <50707000+LysWalkorkill@users.noreply.github.com>
2021-12-12 17:05:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import glob, random
 import matplotlib.image as mpimg
+import SessionState
 from tensorflow import keras
 from tensorflow.keras import layers
 
@@ -16,14 +17,17 @@ st.set_page_config(layout="wide")
 input_col, AI_col = st.columns([1, 2])
 good_tree = ["Azadirachta Indica","Carissa Carandas", "Ficus Religiosa"]
 
-random_image = []
+#random_image = []
+random_image = SessionState.get(img=[])
 i = 1
 while i < 10:
 	file_path_type = ["./leaf dataset/Azadirachta Indica (Neem)/*.jpg", "./leaf dataset/Carissa Carandas (Karanda)/*.jpg", "./leaf dataset/Ficus Religiosa (Peepal Tree)/*jpg"]
 	images = glob.glob(random.choice(file_path_type))
-	random_image.insert(0, random.choice(images))
+	random_image.img.insert(0, random.choice(images))
 	i += 1
-state.pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image) + 1)])
+pick_img = SessionState.get(img=[])
+pick_img.img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image.img) + 1)])
+#pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image) + 1)])
 
 def tree(witch_tree):
     if witch_tree == 'A':
@@ -45,7 +49,7 @@ def main():
 
         for x in range(3):
             for y in range(3):
-                img = mpimg.imread(random_image[y + x * 3])
+                img = mpimg.imread(random_image.img[y + x * 3])
                 ax[x, y].imshow(img)
                 #ax[x, y].set_title(y + x * 3 + 1)
                 ax[x, y].axis('off')
