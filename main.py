daa2a9675dae4a3578d675e35c5a83b2e06507dc
romain <romain.guillaume@epitech.eu>
2021-12-12 05:01:53 +0100

    trying to select and display a specific image

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ st.markdown("<style>.element-container{opacity:1 !important}</style>", unsafe_al
 result = False
 
 def main():
-    if result!:
+    if not result:
         with input_col:
             st.header('Input part')
             st.write('Select the picture to test')
