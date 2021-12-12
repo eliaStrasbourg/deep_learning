c97070b74122a6ae921a318721f1bcba63f2ed2d
romain <romain.guillaume@epitech.eu>
2021-12-12 04:28:30 +0100

    trying to select and display a specific image

--- main.py
+++ main.py
@@ -14,32 +14,36 @@ from tensorflow.keras import layers
 st.set_page_config(layout="wide")
 C1, C2, C3 = st.columns(3)
 
-random_image = []
-for i in range(1, 11):
-    file_path_type = ["./leaf dataset/Azadirachta Indica (Neem)/*.jpg", "./leaf dataset/Carissa Carandas (Karanda)/*.jpg", "./leaf dataset/Ficus Religiosa (Peepal Tree)/*jpg"]
-    images = glob.glob(random.choice(file_path_type))
-    random_image.insert(0, random.choice(images))
+def main():
+    random_image = []
+    for i in range(1, 11):
+        file_path_type = ["./leaf dataset/Azadirachta Indica (Neem)/*.jpg", "./leaf dataset/Carissa Carandas (Karanda)/*.jpg", "./leaf dataset/Ficus Religiosa (Peepal Tree)/*jpg"]
+        images = glob.glob(random.choice(file_path_type))
+        random_image.insert(0, random.choice(images))
 
-with C2:
-    st.markdown("<h1 style='text-align: center; color: white;'>Input part</h1>", unsafe_allow_html=True)
-    #st.header('Input part')
-    st.write('Select the picture to test')
-    #pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image))])
-    st.write('mosaic for the picture')
-    #st.image(random_image, width=100)
-    
-    selected_image = []
-    st.subheader('select one image')
-    for x in range(1, len(random_image)):
-        st.image(random_image[x], caption=x, width=100)
-        selected_image = st.checkbox(random_image[x])
-        if selected_image:
-            selected_image.append(random_image[x])
+    with C2:
+        st.markdown("<h1 style='text-align: center; color: white;'>Input part</h1>", unsafe_allow_html=True)
+        #st.header('Input part')
+        st.write('Select the picture to test')
+        #pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image))])
+        st.write('mosaic for the picture')
+        #st.image(random_image, width=100)
+        
+        selected_image = []
+        st.subheader('select one image')
+        for x in range(1, len(random_image)):
+            st.image(random_image[x], caption=x, width=100)
+            selected_image = st.checkbox(random_image[x])
+            if selected_image:
+                selected_image.append(random_image[x])
 
-    submit = st.button('submit image')
-    if submit:
-        st.image(selected_image, width=100)
+        submit = st.button('submit image')
+        if submit:
+            st.image(selected_image, width=100)
 
+
+if __name__ == "__main__":
+    main()
 
 
 #if pick_img:
