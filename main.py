ba098ab0eb497d3817abb9b4a5344dfce4f32170
romain <romain.guillaume@epitech.eu>
2021-12-12 04:58:56 +0100

    trying to select and display a specific image

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from tensorflow.keras import layers
 st.set_page_config(layout="wide")
 input_col, AI_col = st.columns([1, 3])
 st.markdown("<style>.element-container{opacity:1 !important}</style>", unsafe_allow_html=True)
-result = false
+result = False
 
 def main():
     if not result:
