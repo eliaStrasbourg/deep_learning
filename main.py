36727c76a0726742151c8f666177c87e08a53985
LysWalkorkill <50707000+LysWalkorkill@users.noreply.github.com>
2021-12-12 16:52:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,7 @@ while i < 10:
 	file_path_type = ["./leaf dataset/Azadirachta Indica (Neem)/*.jpg", "./leaf dataset/Carissa Carandas (Karanda)/*.jpg", "./leaf dataset/Ficus Religiosa (Peepal Tree)/*jpg"]
 	images = glob.glob(random.choice(file_path_type))
 	random_image.insert(0, random.choice(images))
-	++i
-	st.write(i)
+	i += 1
 pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image) + 1)])
 
 def tree(witch_tree):
