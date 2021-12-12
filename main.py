cd0ecda977a727727d658c66cd2693b64941c2b4
romain <romain.guillaume@epitech.eu>
2021-12-12 05:06:42 +0100

    trying to select and display a specific image

--- main.py
+++ main.py
@@ -16,31 +16,29 @@ input_col, AI_col = st.columns([1, 3])
 st.markdown("<style>.element-container{opacity:1 !important}</style>", unsafe_allow_html=True)
 
 def main():
-    value = False
-    if not value:
-        with input_col:
-            st.header('Input part')
-            st.write('Select the picture to test')
-            random_image = []
-            for i in range(1, 10):
-                file_path_type = ["./leaf dataset/Azadirachta Indica (Neem)/*.jpg", "./leaf dataset/Carissa Carandas (Karanda)/*.jpg", "./leaf dataset/Ficus Religiosa (Peepal Tree)/*jpg"]
-                images = glob.glob(random.choice(file_path_type))
-                random_image.insert(0, random.choice(images))
-            pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image) + 1)])
-            st.write('mosaic for the picture')
-            st.image(random_image, width=100)
-            st.image(random_image[pick_img - 1])
+    with input_col:
+        st.header('Input part')
+        st.write('Select the picture to test')
+        random_image = []
+        for i in range(1, 10):
+            file_path_type = ["./leaf dataset/Azadirachta Indica (Neem)/*.jpg", "./leaf dataset/Carissa Carandas (Karanda)/*.jpg", "./leaf dataset/Ficus Religiosa (Peepal Tree)/*jpg"]
+            images = glob.glob(random.choice(file_path_type))
+            random_image.insert(0, random.choice(images))
+        pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image) + 1)])
+        st.write('mosaic for the picture')
+        st.image(random_image, width=100)
+        st.image(random_image[pick_img - 1])
 
-            result = st.button('Test AI')
-            if result:
-                value = True
+        result = st.button('Test AI')
+        if result:
+            t.image(random_image[pick_img - 1])
 
-    if value:
+    if result:
         with AI_col:
             st.header('Result of the analyze')
             st.write('For this picture:')
-            st.image(random_image[pick_img - 1])
             st.write('display the picture')
+            st.image(random_image[pick_img - 1], width=200)
             st.write('Our AI deterined this tree:')
             st.write('The real result is :')
             st.write('The result is good / is not')
