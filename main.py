2a2f5a3b46af1167eda1d2621fb1a6d29be85c8e
LysWalkorkill <50707000+LysWalkorkill@users.noreply.github.com>
2021-12-12 17:31:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,15 +17,14 @@ input_col, AI_col = st.columns([1, 2])
 good_tree = ["Azadirachta Indica","Carissa Carandas", "Ficus Religiosa"]
 
 random_image = []
+i = 1
+while i < 10:
+	file_path_type = ["./leaf dataset/Azadirachta Indica (Neem)/*.jpg", "./leaf dataset/Carissa Carandas (Karanda)/*.jpg", "./leaf dataset/Ficus Religiosa (Peepal Tree)/*jpg"]
+	images = glob.glob(random.choice(file_path_type))
+	random_image.insert(0, random.choice(images))
+	i += 1
 pick_img = []
-def randomImageGenerator():
-	i = 1
-	while i < 10:
-		file_path_type = ["./leaf dataset/Azadirachta Indica (Neem)/*.jpg", "./leaf dataset/Carissa Carandas (Karanda)/*.jpg", "./leaf dataset/Ficus Religiosa (Peepal Tree)/*jpg"]
-		images = glob.glob(random.choice(file_path_type))
-		random_image.insert(0, random.choice(images))
-		i += 1
-	pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image) + 1)])
+pick_img = st.sidebar.radio("Which image?", [x for x in range(1, len(random_image) + 1)])
 
 def tree(witch_tree):
     if witch_tree == 'A':
@@ -77,5 +76,4 @@ def main():
 
 
 if __name__ == '__main__':
-	randomImageGenerator()
 	main()
