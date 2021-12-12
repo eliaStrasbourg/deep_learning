d138a53db8cf71b0735ba8644e32ba57a6bee7e3
romain <romain.guillaume@epitech.eu>
2021-12-12 03:47:02 +0100

    trying to select and display a specific image

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ with C2:
      'How would you like to be contacted?',
      (st.image(random_image, width=100))
 
-    st.write('You selected:', option)
+    st.write('You selected:', st.image(option))
 
 
 #if pick_img:
